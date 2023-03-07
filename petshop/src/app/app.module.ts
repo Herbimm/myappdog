a5515a9897c0bfc30df431a8c202ac1f1e79af9d
Herbert Carvalho <herbert.carvalho@cbyk.com.br>
2023-03-07 12:57:05 -0300

    first commit

---
+++ petshop/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { ResetPasswordPageComponent } from './reset-password-page/reset-password-page.component';
+import { SignUpPageComponent } from './sign-up-page/sign-up-page.component';
+import { ProductsPageComponent } from './products-page/products-page.component';
+import { PetsPageComponent } from './pets-page/pets-page.component';
+import { CartPageComponent } from './cart-page/cart-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    LoginPageComponent,
+    ResetPasswordPageComponent,
+    SignUpPageComponent,    
+    ProductsPageComponent,
+    PetsPageComponent,
+    CartPageComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
