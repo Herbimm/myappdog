84762cdaafdea3d18fa2dccc5d208e2f49ce0a09
Herbert Carvalho <herbert.carvalho@cbyk.com.br>
2023-03-10 01:02:49 -0300

    fix aplicação pronta para instalar o toast- point de segurança antes de instalar o toast

--- petshop/src/app/app.module.ts
+++ petshop/src/app/app.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from "@angular/core";
-import { ToastrModule } from "ngx-toastr";
 import { BrowserModule } from "@angular/platform-browser";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { NavbarComponent } from "./components/shared/navbar/navbar.component";
 import { LoginPageComponent } from "./pages/account/login-page/login-page.component";
 import { PetsPageComponent } from "./pages/account/pets-page/pets-page.component";
 import { ResetPasswordPageComponent } from "./pages/account/reset-password-page/reset-password-page.component";
-
 import { FramePageComponent } from "./pages/master/frame-page/frame-page.component";
 import { CartPageComponent } from "./pages/store/cart-page/cart-page.component";
 import { ProductsPageComponent } from "./pages/store/products-page/products-page.component";
@@ -19,8 +16,8 @@ import { ReactiveFormsModule } from "@angular/forms";
 import { LoadingComponent } from './components/shared/loading/loading.component';
 import { MaskDirective } from "./directives/mask.directive";
 import { AuthService } from "./services/auth.services";
-import { SignupPageComponent } from "./pages/account/sign-up-page/sign-up-page.component";
 import { CommonModule } from "@angular/common";
+import { SignupPageComponent } from "./pages/account/sign-up-page/sign-up-page.component";
 
 
 
@@ -46,9 +43,7 @@ import { CommonModule } from "@angular/common";
     AppRoutingModule,
     HttpClientModule,
     ReactiveFormsModule,    
-    CommonModule,
-    BrowserAnimationsModule,    
-    ToastrModule.forRoot(),
+    CommonModule,   
   ],
   providers: [DataService, AuthService],
   bootstrap: [AppComponent]
